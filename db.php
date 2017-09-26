0192ec53e1bcb81f06d4e710aa5d4633959d801a
Maurici Ferreira Junior <31556059+Mauricifj@users.noreply.github.com>
2017-09-26 12:35:41 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,35 +1,39 @@
 <?php
     $server = "us-cdbr-iron-east-05.cleardb.net";
-    $username = "b3efc8441c244c";
-    $password = "70c6d2ac";
+    $user = "b3efc8441c244c";
+    $pass = "70c6d2ac";
     $db = "heroku_ca03b14418567c1";
 
-    // Create connection
+    /* Create connection
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
     if ($conn->connect_error) {
         die("Falha ao conectar com o banco de dados: " . $conn->connect_error);
-    }
+    }*/
+
 //    $date = '1985-12-02';
 //    $time = strtotime($date);
 //    $mysqldate = date('Y-m-d');
 //    $sql = "INSERT INTO Drivers (nameDriver, birthDriver, cpfDriver, carDriver, statusDriver, genderDriver)
 //    VALUES ('John Doe', $mysqldate, '42756522825', 'Sandero', 'ativo', 'M')";
 
-    if($id > 0){
-    	$query="SELECT idDriver FROM drivers";
-    	$r = $conn->query($query) or die($conn->error.__LINE__);
-    	if($r->num_rows > 0) {
-        	$result = $r->fetch_assoc();	
-        	$conn->response($conn->json($result), 200); // send user details
-    	}
+    try {
+        $conn = new PDO("mysql:host=$server;db=myDB", $user, $pass);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully"; 
+    }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage(); 
     }
 
-    if ($conn->query($sql) === TRUE) {
+
+/*   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    }*/
 
     $conn->close();
 ?>
