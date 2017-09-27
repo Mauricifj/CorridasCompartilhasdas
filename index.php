4537e22437e5005a4cd89d8e733447cbdd206a69
Maurici Ferreira Junior <31556059+Mauricifj@users.noreply.github.com>
2017-09-27 11:12:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                             <td>{{ driver.statusDriver }}</td>
                             <td>{{ driver.genderDriver }}</td>
                             <td>
-                                <button ng-click="edit($index)"  class="btn btn-primary btn-xs">Editar</button>
+                                <button ng-click="edit($index, drivers.idDriver)"  class="btn btn-primary btn-xs">Editar</button>
                                 <button ng-click="delete($index, drivers.idDriver)" class="btn btn-danger btn-xs">Apagar</button>
                             </td>
                         </tr>
@@ -156,16 +156,57 @@
                         </li>
                     </ul>
 
-                    <div class="form-group">
-                        <label for="name">Nome</label>
-                        <input ng-model="driver_details.name" type="text" id="name" class="form-control"/>
-                    </div>
+                    <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input ng-model="driver_details.name" id="nameDriver" type="text" class="form-control" name="nameDriver" maxlength="50" placeholder="Nome" required>
+                </div>
+                <div class="spacer">
+                    <hr>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+                    <input ng-model="driver_details.birth" id="birthDriver" type="date" class="form-control" name="birthDriver" min="1900-01-01" max="2000-01-01" required>
+                </div>
+                <div class="spacer">
+                    <hr>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-edit"></i></span>
+                    <input ng-model="driver_details.cpf" id="cpfDriver" type="text" class="form-control" name="cpfDriver" placeholder="CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" title="Formato: xxx.xxx.xxx-xx" required>
+                </div>
+                <div class="spacer">
+                    <hr>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-dashboard"></i></span>
+                    <input ng-model="driver_details.car" id="carDriver" type="text" class="form-control" name="carDriver" placeholder="Modelo do carro" required>
+                </div>
+                <div class="spacer">
+                    <hr>
+                </div>
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-primary active">
+                        <input ng-model="driver_details.status" type="radio" name="statusDriver" id="statusDriver1" autocomplete="off" checked>Status ativo
+                    </label>
+                    <label class="btn btn-primary">
+                        <input ng-model="driver_details.status" type="radio" name="statusDriver" id="statusDriver2" autocomplete="off">Status Inativo
+                    </label>
+                </div>
+                <div class="spacer">
+                    <hr>
+                </div>
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-warning active">
+                        <input ng-model="driver_details.gender" type="radio" name="genderDriver" id="genderDriver1" autocomplete="off" checked>Sexo masculino
+                    </label>
 
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <input type="date" ng-model="driver_details.birth" class="form-control" name="birth">
-                    </div>
-
+                    <label class="btn btn-warning">
+                        <input ng-model="driver_details.gender" type="radio" name="genderDriver" id="genderDriver2" autocomplete="off">Sexo feminino
+                    </label>
+                </div>
+                <div class="spacer">
+                    <br />
+                </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
