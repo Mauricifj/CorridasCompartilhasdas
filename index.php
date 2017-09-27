fe406b25832bec7056753a3bee48cda846c64b5a
Maurici Ferreira Junior <31556059+Mauricifj@users.noreply.github.com>
2017-09-27 11:17:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -138,15 +138,15 @@
     </div>
     <!-- // Modal -->
 
-    <!-- Modal - Update Task -->
-    <div class="modal fade" id="updateDriver" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <!-- Modal - Update Driver -->
+    <div class="modal fade" id="updateDriver1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <h4 class="modal-title" id="myModalLabel">Editar motorista</h4>
+                    <h4 class="modal-title" id="updateDriver">Editar motorista</h4>
                 </div>
                 <div class="modal-body">
 
