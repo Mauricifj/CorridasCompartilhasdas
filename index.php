97243892e917cd96a20a6c43895c2167111031a9
Maurici Ferreira Junior <31556059+Mauricifj@users.noreply.github.com>
2017-10-02 00:03:47 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -25,10 +25,6 @@
             color: #ffffff;
         }
         .bg-3 {
-            background-color: #ffffff; /* White */
-            color: #555555;
-        }
-        .bg-4 {
             background-color: #2f2f2f; /* Black Gray */
             color: #fff;
         }
@@ -75,7 +71,6 @@
 
 <!-- First Container -->
 <div class="container-fluid bg-1 text-center">
-    <h3 class="margin">Where To Find Me?</h3><br>
     <div class="row">
         <div class="col-sm-4">
             <a href="drivers.php"><img src="images/driver.png" class="img-responsive margin center-block" style="width:50%" alt="MOTORISTAS"></a>
@@ -91,20 +86,15 @@
 
 <!-- Second Container -->
 <div class="container-fluid bg-2 text-center">
-    <h3 class="margin">What Am I?</h3>
+    <h3 class="margin">SOBRE NÓS</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
     <a href="#" class="btn btn-default btn-lg">
         <span class="glyphicon glyphicon-search"></span> Search
     </a>
 </div>
 
-<!-- Third Container (Grid) -->
-<div class="container-fluid bg-3 text-center">
-
-</div>
-
 <!-- Footer -->
-<footer class="container-fluid bg-4 text-center">
+<footer class="container-fluid bg-3 text-center">
     <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p>
 </footer>
 
